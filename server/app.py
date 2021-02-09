bb11a260225af17c22e55a9ef3bcf5ed8a63e8fb
Ziqi Shen <ziqi.shen@mail.utoronto.ca>
2021-02-09 13:00:50 -0500

    Update app.py
    Use hello world to see if the server is working

--- server/app.py
+++ server/app.py
@@ -4,8 +4,9 @@ from api.home_handler import home_handler
 
 
 app = Flask(__name__)
-
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
 
 app.register_blueprint(home_handler)
 app.register_blueprint(ping_handler)
-
