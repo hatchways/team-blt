3be411f15365b0524e7e121429e904e2824beda7
Jovialiste82 <52076767+Jovialiste82@users.noreply.github.com>
2021-02-11 14:52:55 -0500

    clearing out comments

--- server/app.py
+++ server/app.py
@@ -9,10 +9,7 @@ from api.home_handler import home_handler
 
 app = Flask(__name__)
 
-####### Added by Philippe ######### start
-######### DB SETUP ######################
 try:
-  # client = pymongo.MongoClient("mongodb+srv://TeamBLT-Admin:<password>@teambltcluster0.5hi8c.mongodb.net/<dbname>?retryWrites=true&w=majority")
   client = pymongo.MongoClient(f"mongodb+srv://TeamBLT-Admin:{os.getenv('MONGODB_PWD')}@teambltcluster0.5hi8c.mongodb.net/TeamBLTCluster0?retryWrites=true&w=majority")
   db = client['test']
   col = db['item']
@@ -21,19 +18,16 @@ except Exception as ex:
   print("****** ERROR - Cannot connect to db ******")
   print(ex)
   print("******************************************")
-################################### end
 
 
 
 
 app.register_blueprint(home_handler)
 app.register_blueprint(ping_handler)
-####### Added by Philippe ######### start
+
 @app.route("/", methods=["GET"])
 def index():
   return f"{os.getenv('TEST_ENV')}... App is running..."
 
 if __name__ == "__main__":
   app.run(port=5000, debug=True)
-
-################################### end
