7712b8e86aecf0f34bb947d7c824b8688e79f8e8
jason1794c <69557789+jason1794c@users.noreply.github.com>
2021-02-11 11:33:23 -0500

    Create Sign Up page
    Created sign up page. Checks for proper inputs such as email structure verification, and password length verification. Also checks for password confirmation. Does not have close button at the moment.

---
+++ client/src/pages/SignUp.js
@@ -0,0 +1,231 @@
+import React, { useState } from 'react'
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+import Modal from 'react-modal';
+import { Link, Redirect } from 'react-router-dom';
+import { modalStyles } from '../themes/theme';
+import '../themes/SignUp.css';
+
+function SignUp() {
+    const [modalIsOpen, setModalIsOpen] = useState(true);
+    const [user, setUser] = useState({
+        name: "",
+        email: "",
+        password: ""
+    });
+    const [passwordTwo, setPasswordTwo] = useState('')
+    const [error, setError] = useState({
+        name: false,
+        email: false,
+        password: false,
+        passwordTwo: false
+    });
+    const [errorMessage, setErrorMessage] = useState({
+        name: '',
+        email: '',
+        password: '',
+        passwordTwo: ''
+    })
+
+    // Handle text input 
+    function handleChange(event) {
+        const field = event.target.name;
+        const tempUser = user;
+        tempUser[field] = event.target.value;
+ 
+        setUser({...tempUser});
+        console.log(user)
+    }
+
+    //Handle password2 input
+    function handlePwTwoChange(event) {
+        setPasswordTwo(event.target.value)
+        console.log(passwordTwo)
+    }
+
+    // On submit, check the sign-up form's criteria.
+    function onSubmit(event) {
+        // Check for a name
+        if (user.name === "") {
+            let err = error;
+            err.name = true;
+            let errorText = errorMessage;
+            errorText.name = 'Please enter your name.'
+
+            setError({...err})
+            setErrorMessage({...errorText})
+        } else {
+            let err = error;
+            err.name = false;
+            let errorText = errorMessage;
+            errorText.name = ''
+
+            setError({...err})
+            setErrorMessage({...errorText})
+        }
+
+        // Check for a valid email
+        if (!validateEmail(user.email)) {
+            let err = error;
+            err.email = true;
+            let errorText = errorMessage;
+            errorText.email = 'Please enter a valid e-mail.'
+
+            setError({...err})
+            setErrorMessage({...errorText})          
+        } else {
+            let err = error;
+            err.email = false;
+            let errorText = errorMessage;
+            errorText.email = ''
+
+            setError({...err})
+            setErrorMessage({...errorText})
+        }
+
+        // Check for a password that is greater than 6 characters in length
+        if (user.password.length < 6) {
+            let err = error;
+            error.password = true;
+            let errorText = errorMessage;
+            errorText.password = 'Passwords must be greater than 6 characters long.'
+
+            setError({...err})
+            setErrorMessage({...errorText})
+        } else {
+            let err = error;
+            err.password = false;
+            let errorText = errorMessage;
+            errorText.password = ''
+
+            setError({...err})
+            setErrorMessage({...errorText})
+        }
+        // Check that the second password is the same as the first password
+        if (passwordTwo !== user.password) {
+            let err = error;
+            err.passwordTwo = true;
+            let errorText = errorMessage;
+            errorText.passwordTwo = 'The passwords do not match.'
+
+            setError({...err})
+            setErrorMessage({...errorText})
+        } else {
+            let err = error;
+            err.passwordTwo = false;
+            let errorText = errorMessage;
+            errorText.passwordTwo = ''
+
+            setError({...err})
+            setErrorMessage({...errorText})
+        }
+
+        // If everything is valid, link back to the sign in page
+        // If any one of the textfields has an error, prevents submission. 
+        if (error.name || error.email || error.password || error.passwordTwo) {
+            event.preventDefault();
+        } 
+    }
+
+    // Validating email function
+    function validateEmail(email) {
+        const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+    }
+
+    return (
+        <Modal isOpen={modalIsOpen} style={modalStyles} >
+            <div className='signUp'>
+                <Typography variant='h4'>Sign Up</Typography>
+
+                <form className='signUp__input' required >
+                    <Typography variant='h6'>Your name:</Typography>
+                    <TextField
+                        name='name'
+                        input='text'
+                        placeholder='Name'
+                        onChange={handleChange}
+                        value={user.name}
+                        variant='outlined'
+                        margin='normal'
+                        required
+                        fullWidth
+                        id='name'
+                        error={error.name}
+                        helperText={errorMessage.name}
+                    />
+
+                    <Typography variant='h6'>Your e-mail address:</Typography>
+                    <TextField
+                        input='text'
+                        placeholder='E-mail'
+                        onChange={handleChange}
+                        value={user.email}
+                        variant='outlined'
+                        margin='normal'
+                        required
+                        fullWidth
+                        id='email'
+                        name='email'
+                        error={error.email}
+                        helperText={errorMessage.email}
+                    />
+
+                    <Typography variant='h6'>Password:</Typography>
+                    <TextField
+                        input='password'
+                        placeholder='Password'
+                        onChange={handleChange}
+                        value={user.password}
+                        variant='outlined'
+                        margin='normal'
+                        fullWidth
+                        name='password'
+                        type='password'
+                        id='password'
+                        inputProps={{
+                            minLength: 6
+                        }}
+                        required
+                        error={error.password}
+                        helperText={errorMessage.password}
+                    />
+
+                    <Typography variant='h6'>Retype your password:</Typography>
+                    <TextField
+                        input='password'
+                        placeholder='Retype your password'
+                        onChange={handlePwTwoChange}
+                        value={passwordTwo}
+                        variant='outlined'
+                        margin='normal'
+                        fullWidth
+                        name='password2'
+                        type='password'
+                        id='password'
+                        inputProps={{
+                            minLength: 6
+                        }}
+                        required
+                        error={error.passwordTwo}
+                        helperText={errorMessage.passwordTwo}
+                    />
+
+                    <Button
+                        type='submit'
+                        variant='contained'
+                        size='large'
+                        onClick={onSubmit}
+                    >CREATE ACCOUNT</Button>
+                </form>
+
+                <div className='signUp__footer'>
+                    Already a member? <Link to='/sign-in' className='link'>Sign In</Link>
+                </div>
+            </div>
+        </Modal>
+    )
+}
+
+export default SignUp
