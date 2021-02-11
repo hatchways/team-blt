355f5da726d393839b355d848368812ef56b5373
jason1794c <69557789+jason1794c@users.noreply.github.com>
2021-02-11 11:27:57 -0500

    Updated App.js
    Included routes to sign-in and sign-up pages. User will be redirected to sign-in page if they are not signed in.

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { MuiThemeProvider } from "@material-ui/core";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect} from "react-router-dom";
 
-import { theme } from "./themes/theme";
-import LandingPage from "./pages/Landing";
+import { theme, modalStyles } from "./themes/theme";
+
+import Modal from 'react-modal';
 
 import "./App.css";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import LandingPage from "./pages/Landing";
+
+
+Modal.setAppElement('#root');
 
 function App() {
+  const [signedIn, setSignedIn] = useState(false);
+  
   return (
     <MuiThemeProvider theme={theme}>
       <BrowserRouter>
-        <Route path="/" component={LandingPage} />
-      </BrowserRouter>
+          <Route exact path="/">
+            {!signedIn ? <Redirect to="/sign-in" /> : <LandingPage />}
+          </Route>
+          <Route exact path="/sign-in" component={SignIn} />
+          <Route exact path="/sign-up" component={SignUp} />
+      </BrowserRouter>    
     </MuiThemeProvider>
   );
 }
