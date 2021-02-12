58ff48ae44f8d4e78b47209edbf8dfd7c3c38062
jason1794c <69557789+jason1794c@users.noreply.github.com>
2021-02-12 14:13:57 -0500

    Update theme.js
    Added different Mui themes. Included styles for the sign-in and sign-up pages.

--- client/src/themes/theme.js
+++ client/src/themes/theme.js
@@ -1,23 +1,57 @@
 import { createMuiTheme } from "@material-ui/core";
-import { green } from "@material-ui/core/colors";
-import LandingPage from "../pages/Landing";
 
 export const theme = createMuiTheme({
-  pallet: {
-    primary: { 
-      main: green[500] 
-    }
-  }
+  palette: {
+    primary: {
+      main: "#DF1B1B",
+    },
+  },
+  overrides: {
+    MuiButton: {
+      root: {
+        borderRadius: "50px",
+        marginBottom: "50px",
+        padding: "20px 50px",
+      },
+    },
+    MuiTextField: {
+      root: {
+        marginBottom: "50px",
+        boxShadow: "0px 5px 17px -7px rgba(0, 0, 0, 0.25)",
+      },
+    },
+  },
 });
 
 export const modalStyles = {
   overlay: {
-    background: 'rgba(36,39,60, 0.5)',
+    background: "rgba(36,39,60, 0.5)",
   },
   content: {
-    width: '40%',
-    height: '70%',
-    margin: 'auto',
-    borderRadius: '5px'
-  }
-}
+    position: "fixed",
+    width: "40%",
+    minHeight: "70vh",
+    margin: "auto",
+    borderRadius: "5px",
+    textAlign: "center",
+  },
+};
+
+export const signInStyles = {
+  display: "flex",
+  flexDirection: "column",
+  height: "60vh",
+  justifyContent: "space-between",
+  margin: "30px",
+};
+
+export const signUpStyles = {
+  display: "flex",
+  flexDirection: "column",
+  margin: "30px",
+};
+
+export const formStyles = {
+  justifyContent: "space-between",
+  margin: "30px",
+};
