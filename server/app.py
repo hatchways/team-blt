c729e1fed57f28a68b358c1e2d624585638167ac
Ziqi Shen <ziqi.shen@mail.utoronto.ca>
2021-02-12 09:51:15 -0500

    login signup functionality probably done
    Draft of login/ signup is done, still need testing.

--- server/app.py
+++ server/app.py
@@ -1,75 +1,123 @@
-from flask import Flask, render_template, jsonify, request
-from wtforms import Form, BooleanField, StringField, PasswordField, validators
+from flask import Flask, render_template, flash, redirect
+from flask_mongoengine import MongoEngine
+from flask_login import LoginManager, UserMixin, current_user, login_user, logout_user, login_required
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import DataRequired, Length, EqualTo
+from config import Config
+from werkzeug.security import generate_password_hash, check_password_hash
 from api.ping_handler import ping_handler
 from api.home_handler import home_handler
-from flask_pymongo import PyMongo
 
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = 'abc'
-mongo = PyMongo(app)
+app.config.from_object(Config)
+
+# Setup Flask-MongoEngine
+db = MongoEngine(app)
+login = LoginManager(app)
 
 app.register_blueprint(home_handler)
 app.register_blueprint(ping_handler)
 
+@login.user_loader
+def load_user(id):
+    return User.objects(username=id).first()
+
+class User(db.Document, UserMixin):
+    username = db.StringField(max_length=20)
+    email = db.EmailField(max_length=60)
+    password = db.StringField(max_length=20)
+    password_hash = db.StringField(max_length=128)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Sign In')
+
+class SignupForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=20)])
+    email = EmailField('Email', validators=[DataRequired(), Length(min=3, max=60)])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=3, max=20), EqualTo('confirm', message='Passwords must match')])
+    confirm = PasswordField('Confirm')
+    submit = SubmitField('Submit')
+
+    def validate_username(self, username):
+        user = User.objects(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+    def validate_email(self, email):
+        user = User.objects(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
+
+
 @app.route('/')
 def index():
-    return '''
-        <form method="POST", action="/user/signup", enctype="multipart/form-data">
-            <input type="text" name="Username">
-            <input type="text" name="Email">
-            <input type="text" name="Password">
-            <input type="text" name="Confirm Password">
-            <input type="submit">
-        </form>
-    '''
+    return render_template('index.html', title='home')
 
-class User:
-    def __init__(self, name, email, password):
-        self._id = ''
-        self.name = name
-        self.email = email
-        self.password = password
-        self.encrypted_password = hash(password)
-        self.is_login = True
-
-    def export(self):
-        return {
-            "_id": "",
-            "name": self.name,
-            "email": self.email,
-            "encrypted_password": self.encrypted_password,
-        }
-
-    def validate(self):
-        if len(str(self.name)) < 6:
-            return False
-        if len(str(self.email)) < 6:
-            return False
-        if len(str(self.password)) < 6:
-            return False
-        return True
-
-
-@app.route('/user/signup', methods=['POST'])
-def signup():
-    user = User(request.form.get("Username"), request.form.get("Email"),
-                request.form.get("Password"))
-    if user.validate():
-        mongo.db.users.insert_one(user.export())
-        return user.export()
-    return 'invalid user!'
-
-@app.route('/login', methods=['POST'])
+@login_required
+@app.route('/login', methods = ['GET','POST'])
 def login():
-    return'''
-        <form method="POST", action="/user/signup", enctype="multipart/form-data">
-            <input type="text" name="Username1">
-            <input type="text" name="Password1">
-            <input type="submit">
-        </form>
-    '''
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.objects(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            return redirect(url_for('login'))
+        login_user(user)
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
 
-@app.route('/login1', methods=['GET','POST'])
-def login_status():
-    '''if db finds user, return success'''
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/signup', methods = ['GET','POST'])
+def signup():
+    form = SignupForm()
+    if form.validate_on_submit():
+        new_user = User(username=form.username.data, email=form.email.data)
+        new_user.set_password(form.password.data)
+        new_user.save()
+    return render_template('signup.html', title='Sign Up', form=form)
+
+
+
+
+# @app.route('/user/signup', methods=['POST'])
+# def signup():
+#     user = User(request.form.get("Username"), request.form.get("Email"),
+#                 request.form.get("Password"))
+#     if user.validate():
+#         mongo.db.users.insert_one(user.export())
+#         return user.export()
+#     return 'invalid user!'
+#
+# @app.route('/login', methods=['POST'])
+# def login():
+#     return'''
+#         <form method="POST", action="/user/signup", enctype="multipart/form-data">
+#             <input type="text" name="Username1">
+#             <input type="text" name="Password1">
+#             <input type="submit">
+#         </form>
+#     '''
+#
+# @app.route('/login1', methods=['GET','POST'])
+# def login_status():
+#     '''if db finds user, return success'''
--- server/config.py
+++ server/config.py
@@ -1,3 +1,6 @@
 import os
 
 TEAM_NAME = os.environ['TEAM_NAME']
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hi my name is nobody'
