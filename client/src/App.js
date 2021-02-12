87c4c8ea8317419f47ae60ecca1280b73af18c05
jason1794c <69557789+jason1794c@users.noreply.github.com>
2021-02-12 14:11:08 -0500

    Update App.js
    Fixed ThemeProvider. Using Switch routing.

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,33 @@
 import React, { useState } from "react";
-import { MuiThemeProvider } from "@material-ui/core";
-import { BrowserRouter, Route, Switch, Redirect} from "react-router-dom";
+import { BrowserRouter, Redirect, Route, Switch} from "react-router-dom";
 
-import { theme, modalStyles } from "./themes/theme";
-
-import Modal from 'react-modal';
+import { theme } from "./themes/theme";
 
 import "./App.css";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import LandingPage from "./pages/Landing";
+import ThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
 
-
-Modal.setAppElement('#root');
 
 function App() {
   const [signedIn, setSignedIn] = useState(false);
   
   return (
-    <MuiThemeProvider theme={theme}>
+    <ThemeProvider theme={theme}>
       <BrowserRouter>
-          <Route exact path="/">
-            {!signedIn ? <Redirect to="/sign-in" /> : <LandingPage />}
-          </Route>
-          <Route exact path='/sign-in' component={SignIn} />
-          <Route exact path='/sign-up' component={SignUp} />
-      </BrowserRouter>    
-    </MuiThemeProvider>
+          <Switch>
+            <Route exact path='/'>
+              {signedIn 
+                ?  <Route exact path='/' component={LandingPage} /> 
+                : <Redirect to='/sign-in' />
+              }
+            </Route>
+            <Route exact path='/sign-in' component={SignIn} />
+            <Route exact path='/sign-up' component={SignUp} />
+          </Switch>
+      </BrowserRouter> 
+    </ThemeProvider>
   );
 }
 
