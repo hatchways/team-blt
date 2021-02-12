3f8e2820f8efbafe0d2af994067c764c8fb9418e
Ziqi Shen <ziqi.shen@mail.utoronto.ca>
2021-02-12 10:54:30 -0500

    Update config.py
    config server

--- server/config.py
+++ server/config.py
@@ -4,3 +4,5 @@ TEAM_NAME = os.environ['TEAM_NAME']
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hi my name is nobody'
+
+    MONGODB_HOST = "mongodb+srv://TeamBLT-Admin:jcPCzsMV2021HatchwaysTeamBLT@teambltcluster0.5hi8c.mongodb.net/test?retryWrites=true&w=majority"
