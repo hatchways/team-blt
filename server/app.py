1ab7ec1aea8266f9d8f8aaf4afdcf689c65d5be6
Ziqi Shen <ziqi.shen@mail.utoronto.ca>
2021-02-16 02:53:07 -0500

    convert it with only backend
    it works, but needs to separate the files.

--- server/app.py
+++ server/app.py
@@ -1,90 +1,105 @@
-from flask import Flask, render_template, flash, redirect, url_for
+from flask import Flask, render_template, flash, redirect, url_for, request, Response, jsonify
 from flask_mongoengine import MongoEngine
 from flask_login import LoginManager, UserMixin, current_user, login_user, logout_user, login_required
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, ValidationError
 from wtforms.fields.html5 import EmailField
 from wtforms.validators import DataRequired, Length, EqualTo
-from config import Config
-from werkzeug.security import generate_password_hash, check_password_hash
+from flask_bcrypt import generate_password_hash, check_password_hash
+from flask_restful import Resource, Api
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required
 from api.ping_handler import ping_handler
 from api.home_handler import home_handler
+import datetime
+import os
 
 app = Flask(__name__)
-app.config.from_object(Config)
-db = MongoEngine(app)
-login = LoginManager(app)
-
+app.config['MONGODB_HOST']=os.environ['MONGODB_HOST']
+app.config['SECRET_KEY']=os.environ['SECRET_KEY']
+app.config['JWT_SECRET_KEY'] = os.environ['JWT_SECRET_KEY']
 app.register_blueprint(home_handler)
 app.register_blueprint(ping_handler)
+api = Api(app)
+db = MongoEngine(app)
+jwt = JWTManager(app)
+login = LoginManager(app)
+
+class UsersApi(Resource):
+    def get(self):
+        users = User.objects().to_json()
+        return Response(users, mimetype="application/json", status=200)
+
+    def delete(self):
+        user = User.objects.delete()
+        return '', 200
+
+    def post(self):
+        body = request.get_json()
+        user = User(**body)
+        user.hash_password()
+        user.save()
+        username = user.username
+        password = user.password
+        return {'username': str(username)}, 201
+
+class UserApi(Resource):
+    @jwt_required
+    def put(self, username):
+        body = request.get_json()
+        User.objects.get(username=username).update(**body)
+        return '', 200
+    @jwt_required
+    def delete(self, username):
+        user = User.objects.get(username=username).delete()
+        return '', 200
+
+    def get(self, username):
+        user = User.objects.get(username=username).to_json()
+        return Response(user, mimetype="application/json", status=200)
+
+
+class SignupApi(Resource):
+    def post(self):
+        body = request.get_json()
+        user = User(**body)
+        user.hash_password()
+        user.save()
+        username = user.username
+        return {'username': str(username)}, 201
+
+class LoginApi(Resource):
+    @jwt_required
+    def post(self):
+        body = request.get_json()
+        user = User.objects.get(username=body.get('username'))
+        checked = user.check_password(body.get('password'))
+        if not checked:
+            return {'error': 'Email or password invalid'}, 401
+
+        expires = datetime.timedelta(days=7)
+        access_token = create_access_token(identity=str(user.username), expires_delta=expires)
+        return {'token': access_token}, 201
+
+def initialize_routes(api):
+    api.add_resource(UsersApi, '/users')
+    api.add_resource(UserApi, '/users/<username>')
+    api.add_resource(SignupApi, '/signup')
+    api.add_resource(LoginApi, '/login')
+
+initialize_routes(api)
 
 @login.user_loader
 def load_user(id):
     return User.objects(username=id).first()
 
 class User(db.Document, UserMixin):
-    username = db.StringField(max_length=20)
-    email = db.EmailField(max_length=60)
-    password = db.StringField(max_length=20)
-    password_hash = db.StringField(max_length=128)
+    username = db.StringField(required=True, unique=True, min_length=6)
+    email = db.EmailField(required=True, min_length=6)
+    password = db.StringField(required=True, min_length=7)
 
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
+
+    def hash_password(self):
+        self.password = generate_password_hash(self.password).decode('utf8')
 
     def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
-
-    def __repr__(self):
-        return '<User {}>'.format(self.username)
-
-
-class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Sign In')
-
-class SignupForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=6)])
-    email = EmailField('Email', validators=[DataRequired(), Length(min=6)])
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=7), EqualTo('confirm', message='Passwords must match')])
-    confirm = PasswordField('Confirm')
-    submit = SubmitField('Submit')
-
-    def validate_username(self, username):
-        user = User.objects(username=username.data).first()
-        if user is not None:
-            raise ValidationError('Please use a different username.')
-
-    def validate_email(self, email):
-        user = User.objects(email=email.data).first()
-        if user is not None:
-            raise ValidationError('Please use a different email address.')
-
-
-@app.route('/')
-def index():
-    return render_template('index.html', title='home')
-
-@login_required
-@app.route('/login', methods = ['GET','POST'])
-def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.objects(username=form.username.data).first()
-        if user is None or not user.check_password(form.password.data):
-            return redirect(url_for('login')), 401
-        login_user(user)
-        return redirect(url_for('index'))
-    return render_template('login.html', title='Sign In', form=form)
-
-@app.route('/signup', methods = ['GET','POST'])
-def signup():
-    form = SignupForm()
-    if form.validate_on_submit():
-        new_user = User(username=form.username.data, email=form.email.data)
-        new_user.set_password(form.password.data)
-        new_user.save()
-        return redirect(url_for('login')), 201
-    return render_template('signup.html', title='Sign Up', form=form)
+        return check_password_hash(self.password, password)
--- server/config.py
+++ server/config.py
@@ -1,7 +1,3 @@
 import os
 
 TEAM_NAME = os.environ['TEAM_NAME']
-
-class Config(object):
-    SECRET_KEY = 'I am cool as superhero'
-    MONGODB_HOST = "mongodb+srv://TeamBLT-Admin:jcPCzsMV2021HatchwaysTeamBLT@teambltcluster0.5hi8c.mongodb.net/test?retryWrites=true&w=majority"
