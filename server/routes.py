e9525a3e65fb61727bfb01011c2ece54cdced1fe
jason1794c <69557789+jason1794c@users.noreply.github.com>
2021-02-18 11:05:48 -0500

    Update routes.py
    Add new API for the product and list's create, read, update, and delete. Updated @jwt_required to @jwt_required().

--- server/routes.py
+++ server/routes.py
@@ -1,8 +1,11 @@
-from flask import request, Response, make_response
+from flask import request, Response, make_response, jsonify
 from flask_restful import Resource, Api
-from flask_jwt_extended import create_access_token, jwt_required
-from models import User
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+from models.user import User
+from models.product import Product
+from models.list import List
 import datetime
+
 
 class UsersApi(Resource):
     def get(self):
@@ -22,20 +25,21 @@ class UsersApi(Resource):
         password = user.password
         return {'username': str(username)}, 201
 
+
 class UserApi(Resource):
 
-    @jwt_required
+    @jwt_required()
     def put(self, username):
         body = request.get_json()
         User.objects.get(username=username).update(**body)
         return '', 200
 
-    @jwt_required
+    @jwt_required()
     def delete(self, username):
         user = User.objects.get(username=username).delete()
         return '', 200
 
-    @jwt_required
+    @jwt_required()
     def get(self, username):
         user = User.objects.get(username=username).to_json()
         return Response(user, mimetype="application/json", status=200)
@@ -50,8 +54,8 @@ class SignupApi(Resource):
         username = user.username
         return {'username': str(username)}, 201
 
+
 class LoginApi(Resource):
-    @jwt_required
     def post(self):
         body = request.get_json()
         user = User.objects.get(username=body.get('username'))
@@ -60,7 +64,89 @@ class LoginApi(Resource):
             return {'error': 'Email or password invalid'}, 401
 
         expires = datetime.timedelta(days=7)
-        access_token = create_access_token(identity=str(user.username), expires_delta=expires)
+        access_token = create_access_token(
+            identity=str(user.username), expires_delta=expires)
         response = make_response('given access')
         response.set_cookie('access token', access_token, httponly=True)
-        return '', 201
+        return {'token': access_token}, 201
+
+# Creating the empty list
+class CreateProductsListApi(Resource):
+    @jwt_required()
+    def post(self):
+        user_id = get_jwt_identity()
+        body = request.get_json()
+        user = User.objects.get(username=user_id)
+        list_of_products = List(**body, added_by=user)
+        list_of_products.save()
+        list_name = list_of_products.list_title
+        user.update(push__list_of_products=list_of_products)
+        user.save()
+        return {'name': str(list_name)}, 201
+
+# List of products
+class ProductsListApi(Resource):
+    @jwt_required()
+    def get(self, list_title):
+        user_id = get_jwt_identity()
+        user = User.objects.get(username=user_id)
+        list_of_products = List.objects.get(list_title=list_title, added_by=user).to_json()
+        return Response(list_of_products, mimetype="application/json", status=200)
+    
+    @jwt_required()
+    def delete(self,list_title):
+        user_id = get_jwt_identity()
+        user = User.objects.get(username=user_id)
+        list_of_products = List.objects(list_title=list_title, added_by=user)
+        list_of_products.delete()
+        return 'Your list has been deleted.', 200
+
+# Adding individual products to the list
+class AddProductApi(Resource):
+    @jwt_required()
+    def post(self, list_title):
+        user_id = get_jwt_identity()
+        body = request.get_json()
+        user = User.objects.get(username=user_id)
+        user_list = List.objects.get(list_title=list_title, added_by=user)
+        product = Product(**body, added_by=user)
+        product.save()
+        product_name = product.product_name
+        user.update(push__products=product)
+        user.save()
+        user_list.update(push__products=product)
+        user_list.save()
+        return {'name': str(product_name)}, 201
+
+# Individual products
+class ProductApi(Resource):
+    @jwt_required()
+    def put(self, list_title, product_name):
+        user_id = get_jwt_identity()
+        user = User.objects.get(username=user_id)
+        list_of_products = List.objects.get(list_title=list_title, added_by=user).to_json()
+        product = Product.objects.get(product_name=product_name, added_by=user)
+        body = request.get_json()
+        product.update(**body)
+        return 'Product has been updated.', 200
+
+    @jwt_required()
+    def delete(self,list_title, product_name):
+        user_id = get_jwt_identity()
+        user = User.objects.get(username=user_id)
+        list_of_products = List.objects.get(list_title=list_title, added_by=user).to_json()
+        product = Product.objects(product_name=product_name, added_by=user)
+        product.delete()
+        return 'Product has been deleted.', 200
+
+    @jwt_required()
+    def get(self, list_title, product_name):
+        user_id = get_jwt_identity()
+        user = User.objects.get(username=user_id)
+        list_of_products = List.objects.get(list_title=list_title, added_by=user).to_json()
+        product = Product.objects.get(product_name=product_name, added_by=user).to_json()
+        return Response(product, mimetype="application/json", status=200)
+        
+
+
+
