e1f91a0b70a2dc9b2b9df511b54be1bc2024bfe7
jason1794c <69557789+jason1794c@users.noreply.github.com>
2021-02-18 11:22:18 -0500

    Updated path files
    Updated the path files to the APIs.

--- server/app.py
+++ server/app.py
@@ -1,18 +1,15 @@
 from flask import Flask
 from db import initialize_db
 from flask_restful import Api
-from routes import UsersApi, UserApi, SignupApi, LoginApi, ProductApi, ProductsListApi, CreateProductsListApi, AddProductApi
+from api.user_api import UsersApi, UserApi, SignupApi, LoginApi
+from api.products_api import ProductApi, ProductsListApi, CreateProductsListApi, AddProductApi
 from flask_jwt_extended import JWTManager
-from api.ping_handler import ping_handler
-from api.home_handler import home_handler
 import os
 
 app = Flask(__name__)
 app.config['MONGODB_HOST']=os.environ['MONGODB_HOST']
 app.config['SECRET_KEY']=os.environ['SECRET_KEY']
 app.config['JWT_SECRET_KEY'] = os.environ['JWT_SECRET_KEY']
-app.register_blueprint(home_handler)
-app.register_blueprint(ping_handler)
 api = Api(app)
 initialize_db(app)
 jwt = JWTManager(app)
