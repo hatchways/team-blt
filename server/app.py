4042ff61696778eb87e04881f943f6a1d3d249bb
jason1794c <69557789+jason1794c@users.noreply.github.com>
2021-02-19 12:22:48 -0500

    Change "username" to "name"

--- server/app.py
+++ server/app.py
@@ -16,7 +16,7 @@ jwt = JWTManager(app)
 
 def initialize_routes(api):
     api.add_resource(UsersApi, '/users')
-    api.add_resource(UserApi, '/users/<username>')
+    api.add_resource(UserApi, '/users/<name>')
     api.add_resource(SignupApi, '/signup')
     api.add_resource(LoginApi, '/login')
     api.add_resource(ProductsListApi, '/create-list', '/lists', '/lists/<list_title>')
