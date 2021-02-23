52ae4efa5619f546c71c536557fdfda53441047b
jason1794c <69557789+jason1794c@users.noreply.github.com>
2021-02-22 20:39:23 -0500

    Update App.js
    Changed '/' path to be directed towards the UserDashboard component. Removed unnecessary space between code.

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter, Redirect, Route, Switch} from "react-router-dom";
-
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 import { theme } from "./themes/theme";
-
 import "./App.css";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
-import LandingPage from "./pages/Landing";
+import UserDashboard from "./pages/UserDashboard";
 import { ThemeProvider as MuiThemeProvider } from "@material-ui/core/styles";
-
 
 function App() {
   // this can be used to show all the user information in dataset.
@@ -22,16 +19,17 @@ function App() {
   return (
     <MuiThemeProvider theme={theme}>
       <BrowserRouter>
-          <Switch>
-            <Route exact path='/'>
-              {signedIn
-                ?  <Route exact path='/' component={LandingPage} />
-                : <Redirect to='/login' />
-              }
-            </Route>
-            <Route exact path='/login' component={SignIn} />
-            <Route exact path='/signup' component={SignUp} />
-          </Switch>
+        <Switch>
+          <Route exact path="/">
+            {signedIn ? (
+              <Route exact path="/" component={UserDashboard} />
+            ) : (
+              <Redirect to="/login" />
+            )}
+          </Route>
+          <Route exact path="/login" component={SignIn} />
+          <Route exact path="/signup" component={SignUp} />
+        </Switch>
       </BrowserRouter>
     </MuiThemeProvider>
   );
