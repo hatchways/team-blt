6ca4f89609049dd3c698142a22a1d29002634476
jason1794c <69557789+jason1794c@users.noreply.github.com>
2021-02-25 12:17:41 -0500

    Create UserContext.js
    Created a user context that fetches the user's profile picture URL. The URL is then set to the imageUrl state which can be accessed in all of the protected routes.

---
+++ client/src/context/UserContext.js
@@ -0,0 +1,49 @@
+import React, { useState, createContext, useEffect } from 'react';
+import Cookies from 'js-cookie';
+
+export const UserModel = createContext();
+
+function UserContext({ children }) {
+    const [imageUrl, setImageUrl] = useState('');
+    const userEmail = Object.keys(Cookies.get());
+    const imageContextValues = {
+        imageUrl: imageUrl,
+        setImageUrl: setImageUrl
+    }
+  
+    /* 
+    The image URL for the profile pic will be fetched from the
+    backend flask server on the first render. The URL will then 
+    be saved into the imageUrl state in the client. Any future
+    changes to the image URL will be saved to the imageUrl state
+    in the client without the need to fetch from the server again.
+    However, the UserSetting.js file has code for sending any new 
+    image URL to the server and setting the imageUrl state in the 
+    client.
+    */
+    useEffect(() => {
+      async function fetchData() {
+        const response = await fetch(`/users/${userEmail}`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "aplication/json",
+            Authorization: `Bearer ${Cookies.get(userEmail)}`,
+          },
+          body: JSON.stringify(),
+        });
+        const user = await response.json(); 
+        console.log(user)
+        setImageUrl(user.profile_pic)
+      }
+      fetchData();
+    }, []);
+    console.log(imageUrl)
+
+    return (
+        <UserModel.Provider value={imageContextValues}>
+            {children}
+        </UserModel.Provider>
+    )
+}
+
+export default UserContext
