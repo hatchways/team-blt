cdd3f2905b726e53e783dd2c77ab04dded28c9dc
jason1794c <69557789+jason1794c@users.noreply.github.com>
2021-02-26 15:32:20 -0500

    Fetch user data from the server upon log in
    When the user logs in, the client will fetch the user model's data and will set the information into local storage as well  as the user model in the client. Two new reducer functions, updateProfilePic and updateProductsLists have been created.

--- client/src/context/actions.js
+++ client/src/context/actions.js
@@ -19,8 +19,32 @@ export async function loginUser(dispatch, loginPayload) {
     let response = await fetch('/login', requestOptions);
 
     if (response.ok) {
+      // email fetched from user's email input in the log in form
       const email = loginPayload.email;
       const token = Cookies.get(email);
+      // Fetch the profile picture URL and the user's list of products from the server
+      async function fetchData() {
+        const response = await fetch(`/users/${email}`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "aplication/json",
+            Authorization: `Bearer ${token}`,
+          },
+          body: JSON.stringify(),
+        });
+        const user = await response.json(); 
+
+        // Set the dispatch type to UPDATE_PROFILE_PIC and update the profile_pic value of the intialState object in reducer.js
+        dispatch({ type: 'UPDATE_PROFILE_PIC', payload: {'profile_pic': user.profile_pic}});
+        localStorage.setItem('profile_pic', JSON.stringify(user.profile_pic));
+        // Set the dispatch typ to UPDATE_PRODUCTS_LISTS and update the list_of_products value of the initial state objecti n reducer.js
+        dispatch({ type: 'UPDATE_PRODUCTS_LISTS', payload: {'list_of_products': user.list_of_products}});
+        localStorage.setItem('list_of_products', JSON.stringify(user.list_of_products));
+      };
+      // Call the fetchData() function to fetch the user's data
+      fetchData()
+
+      // Set the dispatch type to LOGIN_SUCCESS and update the email and token of the initialState object in reducer.js
       dispatch({ type: 'LOGIN_SUCCESS', payload: {'email':email, 'token': token}});
       localStorage.setItem('email', JSON.stringify(email));
       localStorage.setItem('token', JSON.stringify(token));
@@ -45,4 +69,22 @@ export async function logout(dispatch) {
   localStorage.removeItem('email');
   localStorage.removeItem('token');
   localStorage.removeItem('login');
+}
+
+/* 
+The updateProfilPic function's dispatch parameter sets the action type to
+'UPDATE_PROFILE_PIC'. The updated image url from UserSetting.js will then
+be the new value for the profile_pic attribute of the user object.
+*/
+export async function updateProfilePic(dispatch, imageUrl) {
+  dispatch({ type: 'UPDATE_PROFILE_PIC', payload: { 'profile_pic': imageUrl }});
+};
+
+/* 
+The updateProductsLists function's dispatch parameter sets the action type
+to 'UPDATE_PRODUCTS_LISTS'. When the user adds a new list, the list_of_products
+attribute's list will be updated to include the new list.
+*/
+export async function updateProductsLists(dispatch, newList) {
+  dispatch({ type: 'UPDATE_PRODUCTS_LISTS', payload: newList})
 }
