5a99851a46be0c140c735d365df80ce167dc8683
Ziqi Shen <ziqi.shen@mail.utoronto.ca>
2021-02-25 23:21:15 -0500

    material ui testing

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Switch } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { theme } from "./themes/theme";
 import "./App.css";
 import { AuthProvider } from "./context/context";
@@ -10,6 +10,8 @@ import Login from "./pages/SignIn";
 import LoginRoute from "./routes/LoginRoute";
 import SignUpRoute from "./routes/SignUpRoute";
 import SignUp from "./pages/SignUp";
+import Friends from "./features/Friends";
+import Navbar from "./components/header/Navbar";
 
 function App() {
   return (
@@ -28,6 +30,10 @@ function App() {
             <ProtectedRoute
               path={'/'}
               component={LandingPage}
+            />
+            <ProtectedRoute
+              path={'/friends'}
+              component={SignUp}
             />
           </Switch>
         </BrowserRouter>
--- client/src/features/Friends.js
+++ client/src/features/Friends.js
@@ -1,18 +1,31 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Paper from "@material-ui/core/Paper";
 
 const useStyles = makeStyles((theme) => ({
-    
+
 }));
 
 const Friends = (props) => {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <div>
-            Friends Default
-        </div>
-    );
+  return (
+    <Paper>
+      <Tabs
+        // value={}
+        // onChange={handleChange}
+        indicatorColor="primary"
+        textColor="primary"
+        centered
+      >
+        <Tab label="Item One" />
+        <Tab label="Item Two" />
+        <Tab label="Item Three" />
+      </Tabs>
+    </Paper>
+  );
 };
 
 export default Friends;
