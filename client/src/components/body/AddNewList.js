849da4cb26ad2ef3aa5ed970a7225a51e04215dc
jason1794c <69557789+jason1794c@users.noreply.github.com>
2021-02-27 18:01:06 -0500

    Remove dynamic resizing of list cards and changed list title font size

--- client/src/components/body/AddNewList.js
+++ client/src/components/body/AddNewList.js
@@ -15,9 +15,12 @@ const useStyles = makeStyles((theme) => ({
     color: "#DF1B1B",
   },
   root: {
-    maxWidth: 250,
-    flexGrow: "1",
-    height: 325,
+    display: "flex",
+    margin: "auto",
+    width: "250px",
+    height: "350px",
+    marginRight: "1rem",
+    marginTop:"1rem",
   },
   media: {
     height: 250,
@@ -45,7 +48,7 @@ const AddNewList = () => {
             <Typography className={classes.AddIcon} variant="h1">
               +
             </Typography>
-            <Typography gutterBottom variant="caption">
+            <Typography gutterBottom variant="h6" >
               Add New List
             </Typography>
           </CardContent>
