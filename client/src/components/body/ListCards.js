a279c28cc64b1e67fa105d4e5bd57eae3ed5cbee
jason1794c <69557789+jason1794c@users.noreply.github.com>
2021-03-01 12:24:28 -0500

    Update ListCards.js
    Moved the code for the card content to a separate component file called ProductListCard.js.

--- client/src/components/body/ListCards.js
+++ client/src/components/body/ListCards.js
@@ -1,15 +1,11 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import {
-  Grid,
-  Card,
-  CardActionArea,
-  CardContent,
-  CardMedia,
-  Typography,
+  Grid
 } from "@material-ui/core";
 import AddNewList from "../../components/body/AddNewList";
 import { useAuthState } from "../../context/context";
+import ProductListCard from "./ProductListCard";
 
 const useStyles = makeStyles((theme) => ({
   cardContainer: {
@@ -18,50 +14,22 @@ const useStyles = makeStyles((theme) => ({
     justifyContent: "center",
     alignItems: "center",
     flexWrap: "wrap",
-  },
-  root: {
-    width: "250px",
-    height: "350px",
-    marginRight: "1rem",
-    marginTop:"1rem",
-  },
-  media: {
-    height: 250,
-  },
-  content: {
-    textAlign: "center",
-  },
+  }
 }));
 
 const ListCards = () => {
   const classes = useStyles();
   const currentUser = useAuthState();
 
-  return (
-      
-      
+  return ( 
     <Grid className={classes.cardContainer}>    
       {currentUser.list_of_products.map((list) => (
-        <Card className={classes.root} key={list._id.$oid}>
-          <CardActionArea>
-            <CardMedia
-              className={classes.media}
-              image={
-                list.cover_image_url ? list.cover_image_url 
-                : 'https://dealsmateprofilepic.s3.us-east-2.amazonaws.com/default-placeholder.png'
-              }
-              title="Contemplative Reptile"
-            />
-            <CardContent className={classes.content}>
-              <Typography gutterBottom variant="h6" >
-                {list.list_title}
-              </Typography>
-              <Typography gutterBottom variant="caption">
-                {list.products.length} Items
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-        </Card>
+        <ProductListCard
+          key={list._id.$oid}
+          listTitle={list.list_title}
+          cover_image_url={list.cover_image_url}
+          numberOfProducts={list.products.length}
+        />
       ))}
       <AddNewList />
     </Grid>
