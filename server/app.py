b90da8424e8ba656a4aace7859cbc4ed12c9cc67
jason1794c <69557789+jason1794c@users.noreply.github.com>
2021-03-02 15:42:05 -0500

    Change getting a user endpoint to id rather than email
    In order to view a user's profile, their id must be part of the endpoint rather than the email.

--- server/app.py
+++ server/app.py
@@ -16,7 +16,7 @@ jwt = JWTManager(app)
 
 def initialize_routes(api):
     api.add_resource(UsersApi, '/users')
-    api.add_resource(UserApi, '/users/<email>')
+    api.add_resource(UserApi, '/users/<id>')
     api.add_resource(SignupApi, '/signup')
     api.add_resource(LoginApi, '/login')
     api.add_resource(LogoutApi, '/logout')
