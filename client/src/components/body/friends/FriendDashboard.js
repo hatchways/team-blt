980e97977c163813f15f9c9392102cec9446a5a0
jason1794c <69557789+jason1794c@users.noreply.github.com>
2021-03-02 13:56:28 -0500

    Create FriendDashboard

---
+++ client/src/components/body/friends/FriendDashboard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../../header/Navbar'
+
+function FriendDashboard() {
+    return (
+        <>
+            <Navbar />
+        </>
+    )
+}
+
+export default FriendDashboard
