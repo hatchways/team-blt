b99b9d79dbc40aa9ee3d031ecdcf6ca349c690fe
jason1794c <69557789+jason1794c@users.noreply.github.com>
2021-03-02 14:43:33 -0500

    Update FriendDashboard.js

--- client/src/components/body/friends/FriendDashboard.js
+++ client/src/components/body/friends/FriendDashboard.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from '../../header/Navbar'
+import FriendProfile from './FriendProfile'
+import FriendShoppingList from './FriendShoppingList'
 
 function FriendDashboard() {
     return (
         <>
             <Navbar />
+            <FriendProfile />
+            <FriendShoppingList />
         </>
     )
 }
