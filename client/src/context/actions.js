467b07c4a0666add7378f83dac614a1acfa33563
Milan Verma <millan.verma@gmail.com>
2021-03-03 18:46:02 -0700

    Updated list creation and refresh list

--- client/src/context/actions.js
+++ client/src/context/actions.js
@@ -109,3 +109,36 @@ attribute's list will be updated to include the new list.
 export async function updateProductsLists(dispatch, newList) {
   dispatch({ type: 'UPDATE_PRODUCTS_LISTS', payload: newList})
 }
+
+export async function createProductLists(dispatch, token, title, imageUrl){  
+    const response = await fetch(`/create-list`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        list_title: title,
+        cover_image_url: `${imageUrl}`,
+      }),
+    });
+    if (response.ok) {
+      console.log("Success");
+    } else {
+      console.log(response);
+    }
+    
+    const res = await fetch('/lists', {
+      method: "GET",
+      headers: {
+        "Content-Type": "aplication/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(),
+    });
+    const lists = await res.json();
+
+    // Set the dispatch typ to UPDATE_PRODUCTS_LISTS and update the list_of_products value of the initial state objecti n reducer.js
+    dispatch({ type: 'UPDATE_PRODUCTS_LISTS', payload: {'list_of_products': lists}});
+    localStorage.setItem('list_of_products', JSON.stringify(lists));
+}
