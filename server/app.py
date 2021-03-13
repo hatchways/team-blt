ae6eca3b2cca8601bcc5cf0e820dffd3c4e4c9cd
jason1794c <69557789+jason1794c@users.noreply.github.com>
2021-03-13 11:07:58 -0500

    Change ProductApi's GET and DELETE request parameters to take in the product's id rather than the product name
    This change was done in order to ensure proper deletion of a product if the user accidentally adds multiple products of the same name.

--- server/app.py
+++ server/app.py
@@ -29,7 +29,7 @@ def initialize_routes(api):
     api.add_resource(LoginApi, '/login')
     api.add_resource(LogoutApi, '/logout')
     api.add_resource(ProductsListApi, '/create-list', '/lists', '/lists/<list_title>')
-    api.add_resource(ProductApi, '/lists/<list_title>/add-product', '/lists/<list_title>/products', '/lists/<list_title>/products/<product_name>')
+    api.add_resource(ProductApi, '/lists/<list_title>/add-product', '/lists/<list_title>/products', '/lists/<list_title>/products/<id>')
     api.add_resource(OtherUserApi, '/users/<id>')
     api.add_resource(OtherUserProductsListApi, '/users/<id>/lists', '/users/<id>/lists/<list_title>')
     api.add_resource(OtherUserProductApi, '/users/<id>/lists/<list_title>/products')
